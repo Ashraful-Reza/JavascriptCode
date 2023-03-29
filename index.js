d071d3f14a0174b14b6386681f7e7e28f256b8f6
Ashraful-Reza <120293299+Ashraful-Reza@users.noreply.github.com>
2023-03-29 21:25:25 +0600

    Add files via upload
    Lab Works

---
+++ index.js
@@ -0,0 +1,14 @@
+alert("HELLO");
+function multiplyBy()
+     {
+        num1 = document.getElementById("fn").value;
+        num2 = document.getElementById("sn").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+    }
+
+    function divideBy() 
+    { 
+        num1 = document.getElementById("fn").value;
+        num2 = document.getElementById("sn").value;
+        document.getElementById("result").innerHTML = num1 / num2;
+    } 
